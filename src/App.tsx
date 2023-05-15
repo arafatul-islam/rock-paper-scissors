93946717cb4151dc22b58e03ff238df72e1baae5
arafatul-islam <marafatbs@gmail.com>
2023-05-15 21:05:15 +0600

    rock-paper-scissors setup done

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>
+        <h1>Roct, Paper, Scissors</h1>
+        <p>React-TypeScript-Testing Library</p>
+      </div>
+      {/* <ScoreAndResult/>
+    <ChooseAndPlay/> */}
+    </div>
+  );
+}
+
+export default App;
